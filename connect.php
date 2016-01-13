9b251cedab1bb7feb89cfd1691ee468d5fe18ae1
yadav-dharmendra <dharmendra402@gmail.com>
2016-01-13 23:59:40 +0530

    updated db name to staging db

--- connect.php
+++ connect.php
@@ -12,7 +12,7 @@ if($_SERVER['SERVER_ADDR']!='127.0.0.1' && $_SERVER['HTTP_HOST']!='localhost'){
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
-    //echo $mysqli->host_info . "\n";
+    //echo $mysqli->host_info . "\n";;
     
     $mysqli = new mysqli("127.0.0.1", "root", "", "parts_ordering", 3306);
     if ($mysqli->connect_errno) {
